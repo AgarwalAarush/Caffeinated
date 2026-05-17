52f9d3c78e5fc989f5329617f6d15e9f77c36f7a
Aarush Agarwal <aarushaga@gmail.com>
2026-05-17 00:14:32 -0700

    Initial Commit

---
+++ Caffeinated/CaffeinatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeinatedApp.swift
+//  Caffeinated
+//
+//  Created by Aarush Agarwal on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct CaffeinatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
